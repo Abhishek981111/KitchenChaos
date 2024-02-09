c352d8225b5995f8c59bb92d2ed32da602189b13
Abhishek Kadam <kadamabhishek981111@gmail.com>
2024-02-09 22:54:02 +0530

    Smooth rotation for character

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -33,6 +33,9 @@ public class PlayerController : MonoBehaviour
         Vector3 moveDir = new Vector3 (inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        // For smooth rotations
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
         Debug.Log(Time.deltaTime);
     }
 }
