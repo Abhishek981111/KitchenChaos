42da7fb912fb0290daa69bc789aacb3e24348678
Abhishek Kadam <kadamabhishek981111@gmail.com>
2024-04-12 19:59:55 +0530

    Added cutting progress UI with cutting animation to the cuttingCounter using worldCanvas

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -1,9 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CuttingCounter : BaseCounter
 {
+    public event EventHandler<OnProgressChangedEventArgs> OnProgressChanged;
+    public class OnProgressChangedEventArgs : EventArgs
+    {
+        public float progressNormalized;
+    }
+
+    public event EventHandler OnCut;
 
     [SerializeField] private CuttingRecipeSO[] cuttingRecipeSOArray;
     private int cuttingProgress;
@@ -21,6 +29,12 @@ public class CuttingCounter : BaseCounter
                     //Player is carrying something that can be cut
                     player.GetKitchenObject().SetKitchenObjectParent(this);
                     cuttingProgress = 0;
+
+                    CuttingRecipeSO cuttingRecipeSO = GetCuttingRecipeSOWithInput(GetKitchenObject().GetKitchenObjectsSO());
+                    
+                    OnProgressChanged?.Invoke(this, new OnProgressChangedEventArgs{
+                        progressNormalized = (float)cuttingProgress / cuttingRecipeSO.cuttingRecipeMax
+                    });
                 }
             }
             else
@@ -49,7 +63,14 @@ public class CuttingCounter : BaseCounter
         {
             //There is a kitchen Object here and it can be cut 
             cuttingProgress++;
+
+            OnCut?.Invoke(this, EventArgs.Empty);
+            
             CuttingRecipeSO cuttingRecipeSO = GetCuttingRecipeSOWithInput(GetKitchenObject().GetKitchenObjectsSO());
+            
+            OnProgressChanged?.Invoke(this, new OnProgressChangedEventArgs{
+                progressNormalized = (float)cuttingProgress / cuttingRecipeSO.cuttingRecipeMax
+            });
 
             if(cuttingProgress >= cuttingRecipeSO.cuttingRecipeMax)
             {
