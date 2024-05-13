94a110888158e8a8fbc6852f90b5999bceb0f608
Abhishek Kadam <kadamabhishek981111@gmail.com>
2024-05-13 16:17:27 +0530

    Interacting and updating plates visuals with the help of unity events

--- Assets/Scripts/Counters/PlatesCounter.cs
+++ Assets/Scripts/Counters/PlatesCounter.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class PlatesCounter : BaseCounter
 {
     public event EventHandler OnPlateSpawned;
+    public event EventHandler OnPlateRemoved;
+
 
     [SerializeField] private KitchenObjectsSO plateKitchenObjectSO;
 
@@ -28,6 +30,23 @@ public class PlatesCounter : BaseCounter
                 platesSpawnedAmount++;
 
                 OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public override void Interact(Player player)
+    {
+        if(!player.HasKitchenObject())
+        {
+            //player is empty handed
+            if(platesSpawnedAmount > 0)
+            {
+                //there's at least one plate here
+                platesSpawnedAmount--;
+
+                KitchenObjects.SpawnKitchenObject(plateKitchenObjectSO, player);
+
+                OnPlateRemoved?.Invoke(this, EventArgs.Empty);
             }
         }
     }
