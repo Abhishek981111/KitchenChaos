81f12867e03f8e621dc432f664ef11cbf47ecf76
Abhishek Kadam <kadamabhishek981111@gmail.com>
2024-06-18 22:53:01 +0530

    Plate recipe complete visual
    Plate recipe complete visual with use of scriptable objects, structs and lists.

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObjects
 {
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs{
+        public KitchenObjectsSO kitchenObjectsSO;
+    }
+
+
     [SerializeField] private List<KitchenObjectsSO> validKitchenObjectSOList;
     private List<KitchenObjectsSO> kitchenObjectsSOList;
 
@@ -29,6 +36,11 @@ public class PlateKitchenObject : KitchenObjects
         else
         {
             kitchenObjectsSOList.Add(kitchenObjectsSO);
+
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs {
+                kitchenObjectsSO = kitchenObjectsSO
+            });
+            
             return true;
         }
     }
