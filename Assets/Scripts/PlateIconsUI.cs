1a5dce52f97de730974dbc9d8a93f5ed694c47b6
Abhishek Kadam <kadamabhishek981111@gmail.com>
2024-06-21 21:19:38 +0530

    Plate World UI full setup

--- Assets/Scripts/PlateIconsUI.cs
+++ Assets/Scripts/PlateIconsUI.cs
@@ -8,6 +8,11 @@ public class PlateIconsUI : MonoBehaviour
     [SerializeField] private Transform iconTemplate;
 
 
+    private void Awake()
+    {
+        iconTemplate.gameObject.SetActive(false);
+    }
+
     private void Start()
     {
         plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
@@ -20,10 +25,17 @@ public class PlateIconsUI : MonoBehaviour
 
     private void UpdateVisual()
     {
+        foreach(Transform child in transform)
+        {
+            if(child == iconTemplate) continue;
+            Destroy(child.gameObject);
+        }
+
         foreach(KitchenObjectsSO kitchenObjectsSO in plateKitchenObject.GetKitchenObjectsSOList())
         {
             Transform iconTransform = Instantiate(iconTemplate, transform);
-            iconTemplate.GetComponent<PlateIconSingleUI>().SetKitchenObjectSO(kitchenObjectsSO);
+            iconTransform.gameObject.SetActive(true); 
+            iconTransform.GetComponent<PlateIconSingleUI>().SetKitchenObjectSO(kitchenObjectsSO); 
         }
     }
 }
