37782ffe79f0885f1a3d6d986d57d48516012c65
Abhishek Kadam <kadamabhishek981111@gmail.com>
2024-07-09 23:57:47 +0530

    SoundManager for sound Sfx managed using Singleton pattern, scriptable objects and unity event system

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -7,6 +7,9 @@ public class DeliveryManager : MonoBehaviour
 {
     public event EventHandler OnRecipeSpawned;
     public event EventHandler OnRecipeCompleted;
+    public event EventHandler OnRecipeSuccess;
+    public event EventHandler OnRecipeFailed;
+
 
     public static DeliveryManager Instance {get; private set; }
 
@@ -79,7 +82,9 @@ public class DeliveryManager : MonoBehaviour
                 {
                     //Player delivered the correct recipe!
                     waitingRecipeSOList.RemoveAt(i);
+
                     OnRecipeCompleted?.Invoke(this, EventArgs.Empty); 
+                    OnRecipeSuccess?.Invoke(this, EventArgs.Empty);
                     return;
                 }
             }
@@ -87,6 +92,7 @@ public class DeliveryManager : MonoBehaviour
 
         //No matches found
         //Player did not deliver a correct recipe
+        OnRecipeFailed?.Invoke(this, EventArgs.Empty);
     }
 
     public List<RecipeSO> GetWaitingRecipeSOList()
