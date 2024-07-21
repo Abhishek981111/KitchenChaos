a481da84ab7b4a647fb6f34f60cbb54809a1afe0
Abhishek Kadam <kadamabhishek981111@gmail.com>
2024-07-21 13:28:05 +0530

    Implementation of Options button on GamePauseUI and Close button on OptionsUI

--- Assets/Scripts/UI/OptionsUI.cs
+++ Assets/Scripts/UI/OptionsUI.cs
@@ -3,37 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class OptionsUI : MonoBehaviour
 {
+    public static OptionsUI Instance { get; private set; }
+
+
     [SerializeField] private Button soundEffectsButton;
     [SerializeField] private Button musicButton;
+    [SerializeField] private Button closeButton;
     [SerializeField] private TextMeshProUGUI soundEffectsText;
     [SerializeField] private TextMeshProUGUI musicText;
 
 
     private void Awake()
     {
+        Instance = this;
+
         soundEffectsButton.onClick.AddListener(() => {
             SoundManager.Instance.ChangeVolume();
             UpdateVisual();
-        });                                                         //Lambda Expression
+        });                                                        //Lambda Expression
 
         musicButton.onClick.AddListener(() => {
-            MusicManager.Instance.ChangeVolume();   
+            MusicManager.Instance.ChangeVolume();                  //Lambda Expression
             UpdateVisual();
-        });                                                         //Lambda Expression
+        });   
+
+        closeButton.onClick.AddListener(() => {
+            Hide();
+        });                                                       //Lambda Expression
     }
 
     private void Start()
     {
+        KitchenGameManager.Instance.OnGameUnpaused += KitchenGameManager_OnGameUnpaused;
         UpdateVisual();
+        Hide();
+    }
+
+    private void KitchenGameManager_OnGameUnpaused(object sender, EventArgs e)
+    {
+        Hide();
     }
 
     private void UpdateVisual()
     {
         soundEffectsText.text = "Sound Effects: " + Mathf.Round(SoundManager.Instance.GetVolume() * 10f);
         musicText.text = "Music: " + Mathf.Round(MusicManager.Instance.GetVolume() * 10f);
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
     }
 
 }
