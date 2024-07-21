144ea623765b55837f5499e7eb51350bdf8497d1
Abhishek Kadam <kadamabhishek981111@gmail.com>
2024-07-21 18:56:32 +0530

    Using PlayerPrefs saving the values of soundEffects and music in OptionsUI

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
+    private const string PLAYER_PREFS_MUSIC_VOLUME = "MusicVolume";
+
     public static MusicManager Instance { get; private set; } 
 
     private AudioSource audioSource;
@@ -13,7 +15,11 @@ public class MusicManager : MonoBehaviour
     private void Awake()
     {
         Instance = this;
+
         audioSource = GetComponent<AudioSource>();
+
+        volume = PlayerPrefs.GetFloat(PLAYER_PREFS_MUSIC_VOLUME, 0.3f);
+        audioSource.volume = volume;
     }
 
     public void ChangeVolume()
@@ -25,6 +31,9 @@ public class MusicManager : MonoBehaviour
             volume = 0f;
         }
         audioSource.volume = volume;
+
+        PlayerPrefs.GetFloat(PLAYER_PREFS_MUSIC_VOLUME, volume);
+        PlayerPrefs.Save();
     }
 
     public float GetVolume()
